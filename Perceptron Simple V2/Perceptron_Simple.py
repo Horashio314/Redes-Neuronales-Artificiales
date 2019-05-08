1895236ecc735e4e3159ee4bfebd5727c3380ac7
Horashio <horacio.orrillo@gmail.com>
2019-05-08 15:05:02 -0400

    Segundo ejemplo del perceptron simple, xor.

--- Perceptron Simple V2/Perceptron_Simple.py
+++ Perceptron Simple V2/Perceptron_Simple.py
@@ -16,6 +16,7 @@ class Perceptron:
         tasaApren_: float
             Tasa de aprendizaje.
     """
+
     def __init__(self, w_= [0, 0], tasaApren_ = 0.1):
         """ 
         Metodo constructor del preceptron, 
@@ -29,14 +30,13 @@ class Perceptron:
         Salida del perceptron, aplica el producto punto entre w (pesos) y x (data).
         
         Parametros:
-            x: list, forma [coordenada x, coordenada y]
+            x: list, forma [valor 1, valor 2]
                 Data que se esta analizando.
 
         Retorna:
-            int: Si el producto punto es mayor o igual a cero devuelve '1' de lo contrario '-1'
+            int: Si el producto punto es mayor o igual a uno (1) devuelve '1' de lo contrario '0'
         """
         y = (x[0] * self.w[0]) + (x[1] * self.w[1])     # Producto punto entre w y x.
-        print("y", y)
         
         if y >= 1:
             return 1
@@ -53,10 +53,10 @@ class Perceptron:
                 w(t): Es el peso para la iteracion actual de aprendizaje.
                 tasaApren: Tasa de aprendizaje.
                 error: (respuesta deseada) - (respuesta del perceptron).
-                x: Coordenada actual.
+                x: Valor actual.
 
         Parametros:
-            x: list, forma [coordenada x, coordenada y]
+            x: list, forma [valor 1, valor 2]
                 Data que se esta analizando.
         """
         self.w[0] += self.tasaApren * error * x[0]
@@ -69,7 +69,7 @@ class Perceptron:
 
         Parametros:
             data: list, forma [[x1, y1, resp1], [x2, y2, resp2], ... , [xn, yn, respn]]
-                Vector con los datos, cada uno debe tener la forma, coordenada x, coordenada y e respuesta deseada.
+                Vector con los datos, cada uno debe tener la forma, valor 1, valor 2 y respuesta deseada.
         """
         aprendio = False                        # Determina si el perceptron aprendio segun el criterio.
         iteracion = 0                           # Nunero de iteracion que le tomo al perceptron aprender.
@@ -82,49 +82,31 @@ class Perceptron:
 
                 if x[2] != r:                           # Si la respuesta no es la deseada.
                     error = x[2] - r                    # El error en la iteracion se actualiza a: respuesta deseada - respuesta obtenida.
-                    print("x", x ,"error", error)
                     self.actualizarPesos(x, error)      # Se actualiza los pesos con el dato y el error de la iteracion.
                     globalError += abs(error)           # Se actualiza el error general del perceptron.
 
-                
-                print("w", self.w)
-                
-
             iteracion += 1                              # Se contabiliza la iteracion para el criterio de aprendizaje.
-
-            print("globalError", globalError)
-            print("")
             
             if globalError == 0.0 or iteracion >= 1000:      # Criterio de salida: si el error general es 0, o la iteracion de apredndizaje sobre el 100.
-                print("Iteraciones {}".format(iteracion))   # Se imprime las iteraciones necesarias para aprender.
                 aprendio = True                             # Salida del perceptron.
 
-def datosGenerados(n):
+def datosGenerados():
     """
     Metodo encargado de generar un conjunto de datos de prueba, linealmente separables, 
     con la siguiente forma:
         [[x1, y1, resp1], [x2, y2, resp2], ... , [xn, yn, respn]]
 
         Donde:
-            xn: Representa la coordenada X.
-            yn: Representa la coordenada Y.
+            xn: Representa el valor 1.
+            yn: Representa el valor 2.
             respn: Representa la etiqueta de la muestra.
-
-    Parametros:
-        n: int
-            Numero de datos que se desea generar.
 
     Retorna:
         list: Lista con los datos con la siguiente forma: 
-            [[x1, y1, resp1], [x2, y2, resp2], ... , [xn, yn, respn]]
+            [[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
     """
-    xb = (rand(n)*2-1)/2-0.5
-    yb = (rand(n)*2-1)/2+0.5
-    xr = (rand(n)*2-1)/2+0.5
-    yr = (rand(n)*2-1)/2-0.5
+
     datos = []
-        
-    #for i in range(len(xb)):
     datos.append([0,0,0])
     datos.append([0,1,0])
     datos.append([1,0,0])
@@ -133,10 +115,10 @@ def datosGenerados(n):
     return datos
 
 if __name__ == "__main__":
-    datosEntrenamiento = datosGenerados(30)             # Se genera los datos de prueba con los que entrenara el perceptron.
+    datosEntrenamiento = datosGenerados()               # Se genera los datos de prueba con los que entrenara el perceptron.
     perceptron = Perceptron()                           # Se instancia del perceptron.
     perceptron.entrenamiento(datosEntrenamiento)        # Se entrena el perceptron con los datos de prueba.
-    datosPrueba = datosGenerados(20)                    # Se genera los datos con los que probara el perceptron.
+    datosPrueba = datosGenerados()                      # Se genera los datos con los que probara el perceptron.
 
     # Se prueba el perceptron con los datos de prueba.
     for x in datosPrueba:                               # Se recorre los datos de prueba.
@@ -145,15 +127,4 @@ if __name__ == "__main__":
         if r != x[2]:                                   # Verificamos si la respuesta no es correcta.  
             print ('error')                             # Si no es correcta, imprimimos 'error', no se agrega el punto a la grafica.
         else:                                      
-            print("[",x[0],",",x[1],"] -----> ", x[2])                         # Si la respuesta es 1, lo pintamos de azul.
-    #    else:
-    #        plot(x[0],x[1],'or')                        # De lo contrario, lo pintamos de rojo.
-
-    # Se gráfica una línea de separación, la cual es ortogonal a w.
-    #n = norm(perceptron.w)
-    #ww = perceptron.w/n
-    #ww1 = [ww[1],-ww[0]]
-    #ww2 = [-ww[1],ww[0]]
-    #plot([ww1[0], ww2[0]],[ww1[1], ww2[1]],'--k')
-    #show()
-
+            print("[",x[0],",",x[1],"] -----> ", x[2])      # De lo contrario, la respuesta es correcta y se imprime.
